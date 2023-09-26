89dd3a50f7db817245959c9ec4d6198164bc7145
MehrozeNadeem <mehroznadeem77@gmail.com>
2023-09-26 11:49:43 +0500

    Insta_like_button

---
+++ insta_like.js
@@ -0,0 +1,13 @@
+let con = document.querySelector("#container")
+let heart = document.querySelector("i")
+
+con.addEventListener("dblclick",function(){
+    heart.style.transform =  "translate(-50%,-50%) scale(1)"
+    heart.style.opacity = 1
+    setTimeout(function(){
+        heart.style.opacity = 0
+    },1000)
+    setTimeout(function(){
+        heart.style.transform =  "translate(-50%,-50%) scale(1)"
+    },2000)
+})
